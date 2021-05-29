db032afbfc7cb1d344af2242c5199f06dd5b30b3
JakesTechSpace <techlead@jakestechspace.com>
2021-05-29 16:22:15 -0400

    sample code

---
+++ handler.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const axios = require("axios");
+
+module.exports.checkGasFees = async (event) => {
+  // Construct url from env variables
+  const url = `${process.env.API_URL}?api-key=${process.env.API_KEY}`;
+
+  // Make request
+  const response = await axios.get(url);
+  
+  // Destructure values from response
+  const {
+    fast,
+    fastest,
+    safeLow, 
+    average,
+  } = response.data;
+
+  // Divide by 10 to convert to gwei
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      ...response.data,
+      fast: fast / 10,
+      fastest: fastest / 10,
+      safeLow: safeLow / 10, 
+      average: average / 10
+    })
+  };
+};
